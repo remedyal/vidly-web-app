e8631a000e6ad03a097360c5baf3853e9c6946f3
Emeraldy Widiyadi <emeraldy.widiyadi@xtremax.com>
2020-01-06 18:51:20 +0800

    Exercise until Section 2, lesson 12 (Convention-based Routing).

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -14,6 +14,14 @@ namespace Vidly
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                "MoviesByReleaseDate",
+                "movies/released/{year}/{month}",
+                new { controller = "Movies", action = "ByReleaseDate" },
+                //new { year = @"\d{4}", month = @"\d{2}" }
+                new { year = @"2015|2016", month = @"\d{2}" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
